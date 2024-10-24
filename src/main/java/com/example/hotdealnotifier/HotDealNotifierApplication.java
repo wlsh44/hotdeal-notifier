4c8fb44d388f137758b3138b2e9799f4a11f7494
wlsh44 <k87913j@gmail.com>
2024-10-24 16:05:51 +0900

    first commit

---
+++ src/main/java/com/example/hotdealnotifier/HotDealNotifierApplication.java
@@ -0,0 +1,12 @@
+package com.example.hotdealnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotDealNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotDealNotifierApplication.class);
+    }
+}
